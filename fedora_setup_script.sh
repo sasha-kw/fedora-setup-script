11b2cf9c2622b042543bb9122601efa4ba4d12bc
Sasha <me@sashakw.com>
2023-03-03 03:39:37 +1100

    Update fedora_setup_script.sh

--- fedora_setup_script.sh
+++ fedora_setup_script.sh
@@ -133,6 +133,7 @@ sudo dnf install -y vim-gitgutter.noarch # git vim plugin
 sudo dnf install -y @virtualization # virtualisation tools
 sudo dnf install -y rpi-imager.x86_64 # raspberry pi imager
 sudo dnf install -y gqrx.x86_64 # SDR application
+sudo dnf install -y bubblewrap.x86_64 # sandbox application
 pip3 install --user cfn-flip # switch between json and yml cfn
 
 # set vim as default text editor
